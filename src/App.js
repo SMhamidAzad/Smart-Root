93df5fc91b0166f215b5e8a3f813144ae05babfb
SMhamidAzad <hamidazad020@gmail.com>
2022-04-16 21:31:51 +0600

    setup bootstrap react router header banner part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Shared/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+       <Header></Header>
+       <Routes>
+         <Route path='/' element={<Home></Home>}></Route>
+       </Routes>
     </div>
   );
 }
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
-        </div>
-    );
+        <Navbar style={{ backgroundColor: '#040924' }} className='py-3' collapseOnSelect expand="lg" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home" className='fw-bold fs-4' style={{ color: '#E86C53' }}>Classic Watch</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link className='fw-bold' as={CustomLink} to='/'>HOME</Nav.Link>
+                        <Nav.Link className='fw-bold' as={CustomLink} to='/reviews'>REVIEWS</Nav.Link>
+                        <Nav.Link className='fw-bold' as={CustomLink} to='/dashboard'>DASHBOARD</Nav.Link>
+                        <Nav.Link className='fw-bold' as={CustomLink} to='/blogs'>BLOGS</Nav.Link>
+                        <Nav.Link className='fw-bold' as={CustomLink} to='/about'>ABOUT</Nav.Link>
+                    </Nav>
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
 };
 
 export default Header;
