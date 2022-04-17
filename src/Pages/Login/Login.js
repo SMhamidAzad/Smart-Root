e907c4344dede5f40abf810caf5427226d111858
SMhamidAzad <hamidazad020@gmail.com>
2022-04-17 09:47:23 +0600

    login and signup form style updated

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,8 +2,31 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is login components</h2>
+        <div className='form-container'>
+            <div className='form'>
+                <h2>Login</h2>
+                <form>
+                    <div className='input-div'>
+                        <label htmlFor="name">Email</label>
+                        <div className='input-field'>
+                            <input type="text" name="email" id="email" />
+                        </div>
+                    </div>
+                    {/* {errors?.email && <p className='text-danger'>❌{errors.email}</p>} */}
+                    <div className='input-div'>
+                        <label htmlFor="password">Password</label>
+                        <div style={{position: "relative"}} className='input-field'>
+                            <input  type="password" name="password" id="password" />
+                            
+                        </div>
+                    </div>
+                    {/* {errors?.password && <p className='text-danger'>❌{errors.password}</p>} */}
+                    
+
+
+                    <button className='submit-btn'>Login</button>
+                </form>
+            </div>
         </div>
     );
 };
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import './SignUp.css'
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>This is sign up components</h2>
+        <div className='form-container'>
+            <div className='form'>
+                <h2>SignUp</h2>
+                <form>
+                    <div className='input-div'>
+                        <label htmlFor="name">Email</label>
+                        <div className='input-field'>
+                            <input type="text" name="email" id="email" />
+                        </div>
+                    </div>
+                    {/* {errors?.email && <p className='text-danger'>❌{errors.email}</p>} */}
+                    <div className='input-div'>
+                        <label htmlFor="password">Password</label>
+                        <div style={{position: "relative"}} className='input-field'>
+                            <input  type="password" name="password" id="password" />
+                            
+                        </div>
+                    </div>
+                    {/* {errors?.password && <p className='text-danger'>❌{errors.password}</p>} */}
+                    <div className='input-div'>
+                        <label htmlFor="confirmPass">Confirm Password</label>
+                        <div className='input-field'>
+                            <input  type="password" name="confirmPass" id="confirmPass" />
+                        </div>
+                    </div>
+
+
+                    <button className='submit-btn'>SignUp</button>
+                </form>
+            </div>
         </div>
     );
 };
