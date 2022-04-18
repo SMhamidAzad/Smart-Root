266b83f7f13ec361ee9ed91029793034bd34c1a6
SMhamidAzad <hamidazad020@gmail.com>
2022-04-18 10:29:57 +0600

    404 style added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import profile from '../../images/Screenshot (242).png'
+import profile from '../../images/profile.png'
 import './About.css'
 
 const About = () => {
@@ -11,7 +11,7 @@ const About = () => {
                 </div>
                 <div className='text-center'>
                     <h2 className='my-3 primary-color'>Hi, I am Syed Md. Abdul Hamid</h2>
-                    <p className='primary-color'>I am Junior Frontend Web Developer. I want to a Full Stack Web Developer within 2022. Everyday, I am learning new thing for improve my development skill. I spend more than 6-8 hours to fullfil my goal in 2022.I am also completing my graduation from Computer Science & Enginnering Depertment. My university name is International Islamic University Chittagong. Please pray for me for completing my goal.Thank you so much for reading about me.</p>
+                    <p className='primary-color'>I am Junior Frontend Web Developer. I want to a Full Stack Web Developer within 2022. Everyday, I am learning new thing for improve my development skill. I spend more than 6-8 hours to fullfil my goal in 2022.I am also completing my graduation from Computer Science & Enginnering Depertment. My university name is International Islamic University Chittagong.Within one year, I will complete my graduation, Inshallah. Please pray for me for completing my goal.Thank you so much for reading about me.</p>
                 </div>
             </div>
         </div>
--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Ops! Your searching page are not found </h2>
-            <h1>404</h1>
+        <div className='error-container'>
+            <h1 className='error-message primary-color'>The page you are looking for is not found!!</h1>
+            <h1 className='error-code'>404!</h1>
+            <Link to='/' className='back-btn'>Back To Home</Link>
         </div>
     );
 };
